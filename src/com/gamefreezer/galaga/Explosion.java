926a20762a7e8c2cb1cde73b44871c02b8103680
Sean Noble <sean.noble.72@gmail.com>
2011-03-17 17:31:35 +0100

    pulled out explosions as own objects, optimised

---
+++ src/com/gamefreezer/galaga/Explosion.java
@@ -0,0 +1,35 @@
+package com.gamefreezer.galaga;
+
+public class Explosion {
+
+    private AnimationFrames animation;
+    boolean isAvailable;
+    private Entity entity;
+
+    public Explosion(SpriteCache spriteCache, String[] images, int[] frameTimes) {
+	animation = new AnimationFrames(spriteCache, images, frameTimes, true);
+	isAvailable = true;
+    }
+
+    public boolean available() {
+	return isAvailable;
+    }
+
+    public void use(Entity entity) {
+	isAvailable = false;
+	animation.reset();
+	this.entity = entity;
+	entity.setExploding(true);
+    }
+
+    public void draw(AbstractGraphics graphics) {
+	animation.draw(graphics, entity.getX(), entity.getY());
+	if (animation.finished()) {
+	    entity.setExploding(false);
+	    entity = null;
+	    isAvailable = true;
+	}
+
+    }
+
+}
---
+++ src/com/gamefreezer/galaga/Explosions.java
@@ -0,0 +1,49 @@
+package com.gamefreezer.galaga;
+
+public class Explosions {
+    private Constants cfg;
+    private SpriteCache spriteCache;
+    private Explosion[] mExplosions;
+
+    public Explosions(SpriteCache spriteCache, Constants cfg) {
+	this.spriteCache = spriteCache;
+	this.cfg = cfg;
+	initExplosions();
+    }
+
+    private void initExplosions() {
+	// TODO magic numbers and fodder for config file
+	mExplosions = new Explosion[cfg.MAX_EXPLOSIONS];
+	for (int i = 0; i < mExplosions.length; i++) {
+	    mExplosions[i] = new Explosion(spriteCache, cfg.EXPL_IMGS,
+		    cfg.EXPL_TIMES);
+	}
+    }
+
+    public void newExplosion(Entity entity) {
+	Explosion explosion = findAvailableExplosion();
+	assert explosion != null : "No free explosions, increase pool size!";
+	if (explosion != null) {
+	    explosion.use(entity);
+	}
+	// too many explosions, drop this one
+    }
+
+    private Explosion findAvailableExplosion() {
+	for (int i = 0; i < mExplosions.length; i++) {
+	    if (mExplosions[i].available()) {
+		return mExplosions[i];
+	    }
+	}
+	return null;
+    }
+
+    public void draw(AbstractGraphics graphics) {
+	for (int i = 0; i < mExplosions.length; i++) {
+	    if (!mExplosions[i].available()) {
+		mExplosions[i].draw(graphics);
+	    }
+	}
+    }
+
+}
